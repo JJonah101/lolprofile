c3bc3dfc96e27168e2849bbd1d664558461ecd22
Tobi <Shiningred1997@googlemail.com>
2022-08-01 20:29:40 +0200

    added style files for components

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 
-window.apiKey = "RGAPI-581d7bb9-ef4f-4414-afb5-01d8f82f20eb";
+window.apiKey = "RGAPI-1cbb2250-d508-414a-bbb4-8825b94773b2";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
